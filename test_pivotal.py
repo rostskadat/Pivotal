576824b003685e689a09f73ee40ba7f442a9c1d8
rostskadat <rostskadat@gmail.com>
2022-11-30 16:30:09 +0100

    improved handling of different functions

--- test_pivotal.py
+++ test_pivotal.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 """
-Launch test of the LCS module
+Launch test of the LCS module.
+
+SYNOPSIS:
+
+test_pivotal.py --username username --password password --form-name "oraContact" --record-id "000000000000005C"
+test_pivotal.py --username username --password password --asr-name "SolWeb" --method-name "ConsultaDetalleSiniestro" --parameters string,es-ES --parameters string,0010027100
 """
 from argparse import ArgumentParser, RawTextHelpFormatter
 import lcs
@@ -17,11 +22,17 @@ def launch_test(args):
     Args:
         args (_type_): _description_
     """
-    # response = lcs.get_form_data(args.username, args.password, 'oraContact', '000000000000005C')
-    parameters = [('string', 'es-ES'), ('string', '0010027100')]
-    response = lcs.execute_asr("username", "password", 'SolWeb', 'ConsultaDetalleSiniestro', parameters)
+    if args.form_name and args.record_id:
+        response = lcs.get_form_data(args.username, args.password, args.form_name, args.record_id)
+    elif args.asr_name and args.method_name:
+        parameters = []
+        for parameter in args.parameters:
+            (type, value) = parameter[0].split(",")
+            parameters.append((type, value))
+        response = lcs.execute_asr(args.username, args.password, args.asr_name, args.method_name, parameters)
+    else:
+        logger.error()
     logger.info(response)
-    return
 
 def parse_command_line():
     parser = ArgumentParser(prog='test_pivotal',
@@ -32,6 +43,16 @@ def parse_command_line():
         '--username', help='The username to access the server', required=False, default=None)
     parser.add_argument(
         '--password', help='The password to access the server', required=False, default=None)
+    parser.add_argument(
+        '--form-name', help='The form to call in order to get a specific record', required=False, default=None)
+    parser.add_argument(
+        '--record-id', help='The record id to get', required=False, default=None)
+    parser.add_argument(
+        '--asr-name', help='The name of the script / asr to call', required=False, default=None)
+    parser.add_argument(
+        '--method-name', help='The method name to call', required=False, default=None)
+    parser.add_argument(
+        '--parameters', help='The parameter of the call', required=False, default=None, type=str, nargs='+', action='append')
     parser.set_defaults(func=launch_test)
     return parser.parse_args()
 
